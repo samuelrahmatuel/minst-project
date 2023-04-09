f7e8d2b443e5653aeca8106c4bd957be77824e3d
samuelrahmatuel <cemtina.ily@gmail.com>
2023-04-10 00:50:02 +0700

    UPUP

--- app.py
+++ app.py
@@ -19,21 +19,26 @@ def predict_output(input_data):
 
 # Function to preprocess the drawn image
 def preprocess_input(img):
-    img = cv2.resize(img, (28, 28))  # Resize the image to 28x28
-    img = img.astype('float32') / 255.0  # Scale the image
-    img = np.expand_dims(img, axis=0)  # Add batch dimension
+    img = cv2.resize(img, (28, 28))
+    img = img.astype('float32') / 255.0
+    img = np.expand_dims(img, axis=0)
     return img
 
 # Define the Streamlit app
 def app():
-    st.title("My Machine Learning App")
+    st.set_page_config(page_title="Handwritten Digit Recognition", page_icon=":pencil2:")
+
+    st.title("Handwritten Digit Recognition")
+    st.markdown("## :pencil2: Draw a digit from 0 to 9")
+
+    st.write("This app uses a trained machine learning model to recognize handwritten digits. Draw a digit (0-9) in the canvas below and click 'Submit' to see the predicted result. *WIP Project*")
 
     # Add a drawing canvas
     canvas_result = st_canvas(
-        fill_color="rgba(0, 0, 0, 1)",  # Canvas background color
-        stroke_width=20,  # Brush size
-        stroke_color="white",  # Brush color
-        background_color="black",  # Background color
+        fill_color="rgba(0, 0, 0, 1)",
+        stroke_width=20,
+        stroke_color="white",
+        background_color="black",
         width=280,
         height=280,
         drawing_mode="freedraw",
@@ -49,9 +54,12 @@ def app():
             input_data = cv2.cvtColor(canvas_result.image_data.astype('uint8'), cv2.COLOR_RGBA2GRAY)
             prediction = predict_output(input_data)
             predicted_digit = np.argmax(prediction)
-            st.write(f"Prediction: {predicted_digit}")
+            st.success(f"Prediction: {predicted_digit}")
         else:
-            st.write("Please draw a digit in the canvas.")
+            st.warning("Please draw a digit in the canvas.")
+            
+    st.write("Next Progress : Train Model more, Implementing Deep Learning for recommendation, Add more features")
+    st.markdown("<p style='text-align: right; font-style: italic;'>Created by: <a href='https://rahmatuelsamuel.com'>Rahmatuel Samuel</a></p>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     app()
